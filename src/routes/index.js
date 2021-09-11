4ac4866a6639bc3ca517f4a454e2c4c6396b5be5
Mariana Andrade <andrade.marianaferreira@gmail.com>
2021-09-11 00:05:30 -0300

    ajustando descrição da API no app.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ router.get('/', (req, res) => {
     res.status(200).json({
         "tittle": "on12-api-final-meu-pet-pe",
         "version": "1.0.0",
-        "mensagem": "API voltada para juntar anjinhos desabrigados com seus tutores"
+        "mensagem": "Bem-vinde a API Meu PET PE, uma API voltada para juntar anjinhos desabrigados com seus novos tutores"
     });
 })
 
